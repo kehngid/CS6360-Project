5d7c8d60846ea2205b3e24c7edee521ae0207143
Jason <77454384+kehngid@users.noreply.github.com>
2023-12-03 18:53:01 -0600

    Added some results
    - Will need to reformat output
    - Need to do two more testing datasets, there were errors the first time

--- Partial_Order/partialOrder.py
+++ Partial_Order/partialOrder.py
@@ -4,6 +4,22 @@ import numpy as np
 
 import Functions.graph as graph
 
+def write_list_to_file(file_path, my_list):
+    """
+    Write elements of a list to a text file.
+
+    Parameters:
+    - file_path (str): The path of the text file.
+    - my_list (list): The list whose elements will be written to the file.
+    """
+    try:
+        with open(file_path, 'w') as file:
+            for item in my_list:
+                file.write(str(item) + '\n')
+        print(f"Elements written to {file_path} successfully.")
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+
 def runPartialOrder(filename, k = 10):
     filename = './' + filename
     nodes = generateSearchSpace(filename)
@@ -20,5 +36,9 @@ def runPartialOrder(filename, k = 10):
         for i in orderedList: 
             print(i)
 
+    write_list_to_file('./Results/Partial_Order/titanicPassangerResults.txt', orderedList)
+    
+
+
 #runPartialOrder('./data/testing.csv')
-runPartialOrder('./data/electricityConsumptionOfEasternChina.csv', 'all')
+runPartialOrder('./data/titanicPassenger.csv', 'all')
